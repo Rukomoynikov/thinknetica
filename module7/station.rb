65dcd201e55192183da084c9e1d4db7e1ed78936
Maksim R <cat@mos-it.com>
2018-01-21 18:27:06 +0300

    Модуль 7
    Исправления по комментариям

---
+++ module7/station.rb
@@ -0,0 +1,43 @@
+require_relative './shared/validator'
+
+class Station
+  include Validator
+
+  @@instances = []
+
+  attr_reader :name
+
+  def self.all
+    @@instances    
+  end
+
+  def initialize(name)
+    @name = name
+    validate!
+
+    @trains = []
+    @@instances << self
+  end
+
+  def accept_train(train)
+    @trains << train
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+
+  def trains(type = nil)
+    if !type.nil?
+      @trains.select { |train| train.type == type }
+    else
+      @trains
+    end
+  end
+
+  private 
+
+  def validate!
+    raise 'Пустое название' if self.name.nil? or self.name.length.zero?
+  end
+end
